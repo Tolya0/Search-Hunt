86439f208d569d365aba7736f19e451b1078ec4f
Anatoly Kurylin <tolikkurilin01@gmail.com>
2026-05-12 23:42:12 +0300

    add: scene's. connect: atlantafx

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module org.kurilin.recruitment.client {
     requires org.slf4j;
     requires com.google.gson;
     requires org.kurilin.recruitment.shared;
+    requires atlantafx.base;
+    requires static lombok;
 
     opens org.kurilin.recruitment.client to javafx.fxml;
     opens org.kurilin.recruitment.client.controller to javafx.fxml;
